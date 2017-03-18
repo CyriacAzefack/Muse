c5a2d13e5c94f470a7a8550878f084bf8704e3be
Cyriac AZEFACK <cyrahl12@gmail.com>
2017-03-19 00:08:29 +0100

    remove log on error

--- bot_server.js
+++ bot_server.js
@@ -236,7 +236,7 @@ function callSendAPI(messageData) {
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
-      console.error(response);
+      //console.error(response);
       console.error(error);
     }
   });  
