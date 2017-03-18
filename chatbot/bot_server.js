29585b0044792ebdfd5801c0ba23f23c874a0127
Cyriac AZEFACK <cyrahl12@gmail.com>
2017-03-18 21:13:52 +0100

    first deployment

--- chatbot/bot_server.js
+++ chatbot/bot_server.js
@@ -1,11 +1,28 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var app = express();
 
 app.listen(4444, function () {
 
-  console.log('Example app listening on port 4444!dsqdqsd');
+  console.log('Muse Server Started ...');
 });
+
+/*
+//Diffents call
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+*/
+
+app.get('/webhook', function (req, res) {
+
+  console.log(req.quey);
+  if (req.query['hub.verify_token'] === 'facebook_token_on_muse_oklm') {
+    res.send(req.query['hub.challenge']);
+  } 
+  else {
+    res.send('Error, wrong validation token');    
+  }
+  });
+
+
