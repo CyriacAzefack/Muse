4e247dbd847cc8dfa703db337a642ed92e8c8626
Cyriac AZEFACK <cyrahl12@gmail.com>
2017-03-26 22:09:51 +0200

    Add spotify

---
+++ spotify.js
@@ -0,0 +1,44 @@
+/**
+ * Created by I327247 on 26/03/2017.
+ */
+var spotifyWebApi = require('spotify-web-api-node');
+
+const config = require("./config.json");
+
+//credentials settings
+var spotifyApi = new spotifyWebApi({
+    clientId : config.spotify.clientId,
+    clientSecret : config.spotify.clientSecret,
+    redirectUri : config.spotify.redirectUSri
+});
+
+
+// Search tracks whose artist's name contains 'Kendrick Lamar', and track name contains 'Alright'
+/*
+spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar')
+    .then(function(data) {
+        console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name');
+        console.log(data.body.tracks.items);
+    }, function(err) {
+        console.log('Something went wrong!', err);
+    });
+*/
+
+function searchSongAndArtist(songName, artistName) {
+    spotifyApi.searchTracks('track:'+songName+' artist:'+artistName+'')
+        .then(function(data) {
+            console.log('Search tracks by "'+songName+'" in the track name and "'+artistName+'" in the artist name');
+
+            //Now let's build the
+            track = data.body.tracks.items[0];
+
+            return {
+                sample : track.preview_url,
+                song : track.external_urls.spotify,
+                image : track.album.images[0].url
+            }
+        }, function(err) {
+            console.log('Something went wrong!', err);
+        });
+
+}
