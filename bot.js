3bec5a861be91eb505995c6ec9b30f00237a655a
Cyriac AZEFACK <cyrahl12@gmail.com>
2017-03-27 22:18:47 +0200

    local management of memory

--- bot.js
+++ bot.js
@@ -53,13 +53,13 @@ function handleMessage(event) {
                 value = value.replace(/['"]+/g, '');
 
 
-                if (action.slug === "order-music" ) {
+                if (action.slug === "order-music") {
                     recastai.Conversation.setMemory(config.recastai.requestAccessToken, senderID,
-                        { song: { value: value } });
+                        {song: {value: value}});
                 }
-                else if (action.slug === "order-artist" ) {
+                else if (action.slug === "order-artist") {
                     recastai.Conversation.setMemory(config.recastai.requestAccessToken, senderID,
-                        { singer: { value: value } });
+                        {singer: {value: value}});
                 }
 
             }
@@ -67,89 +67,87 @@ function handleMessage(event) {
                 recastai.Conversation.resetMemory(config.recastai.requestAccessToken, senderID);
             }
 
-        });
 
-        //Converese with the bot with the right status of the memory
-		client.textConverse(messageText, { conversationToken: senderID}).then(function(res) {
-			//const reply = res.reply; 		//First reply of the bot
-			const replies = res.replies;	//All the bot replies
-			const action = res.action; 		// Get the current action
-			//const intents = res.intents;
+            //Converese with the bot with the right status of the memory
+            client.textConverse(messageText, {conversationToken: senderID}).then(function (res) {
+                //const reply = res.reply; 		//First reply of the bot
+                const replies = res.replies;	//All the bot replies
+                const action = res.action; 		// Get the current action
+                //const intents = res.intents;
 
 
-			if(messageText === "Reset") {
-				recastai.Conversation.resetMemory(config.recastai.requestAccessToken, senderID);
-				facebook.replyMessage(senderID, "Conversation  remise à zero");
-			}
-			else if(messageText === "Test button") {
-				const options = {
-					messageText: null,
-					title: 		'Kidogo -- Diamond Platnumz',
-					mainUrl: 	'www.google.com',
-					imageUrl: 	'http://www.kizobrax.net/wp-content/uploads/2016/06/diamond-platnumz-and-p-square.jpg',
-					buttonType: 'web_url',
-					buttonTitle:'Listen on Spotify',
-					buttonUrl: 	'https://play.spotify.com/album/3XBqlfR8vyCsbj2n9Ig6nH',
-				};
+                if (messageText === "Reset") {
+                    recastai.Conversation.resetMemory(config.recastai.requestAccessToken, senderID);
+                    facebook.replyMessage(senderID, "Conversation  remise à zero");
+                }
+                else if (messageText === "Test button") {
+                    const options = {
+                        messageText: null,
+                        title: 'Kidogo -- Diamond Platnumz',
+                        mainUrl: 'www.google.com',
+                        imageUrl: 'http://www.kizobrax.net/wp-content/uploads/2016/06/diamond-platnumz-and-p-square.jpg',
+                        buttonType: 'web_url',
+                        buttonTitle: 'Listen on Spotify',
+                        buttonUrl: 'https://play.spotify.com/album/3XBqlfR8vyCsbj2n9Ig6nH',
+                    };
 
-				facebook.replyButton(senderID, options);
-			}
+                    facebook.replyButton(senderID, options);
+                }
 
-			else {
-
+                else {
 
 
 
-				//Promise : Asynchronous manager
-				let promise = Promise.resolve();
-				replies.forEach(function(rep) {
-					promise = promise.then(function() {
-						facebook.replyMessage(senderID, rep).catch(function(err) {
-							console.error("Failed sending : %s", rep);
-							console.error(err);
-						});
 
-					});
-				});
+                    //Promise : Asynchronous manager
+                    let promise = Promise.resolve();
+                    replies.forEach(function (rep) {
+                        promise = promise.then(function () {
+                            facebook.replyMessage(senderID, rep).catch(function (err) {
+                                console.error("Failed sending : %s", rep);
+                                console.error(err);
+                            });
 
-				promise.then(function() {
-					console.log("Messages sending...");
-				}). catch(function(err) {
-					console.error("Message to '%s' Failed!! Check the logs for more details", senderID);
-					console.error(err);
-				});
-			}
-
-			if (action.slug === "order-artist" && action.done) {
-                let song = res.getMemory('song');
-                let artist = res.getMemory('singer');
-
-                if ((song !== null) && (artist !== null) && (action) && (action.done)) {
-
-                    song = song.raw;
-                    artist = artist.raw;
-                    spotify.searchSongAndArtist(song, artist, function (urls) {
-                        const options = {
-                            messageText: null,
-                            title: song.raw + " -- " + artist,
-                            mainUrl: urls.song,
-                            imageUrl: urls.image,
-                            buttonType: 'web_url',
-                            buttonTitle: 'Ecouter un extrait',
-                            buttonUrl: urls.sample,
-                        };
-                        facebook.replyButton(senderID, options);
+                        });
                     });
 
+                    promise.then(function () {
+                        console.log("Messages sending...");
+                    }).catch(function (err) {
+                        console.error("Message to '%s' Failed!! Check the logs for more details", senderID);
+                        console.error(err);
+                    });
                 }
-            }
 
-		}).catch(function(err) {
-			console.error("RecastAI conversation failed!! Check the logs for more details");
-			console.error(err);
-		});
+                if (action.slug === "order-artist" && action.done) {
+                    let song = res.getMemory('song');
+                    let artist = res.getMemory('singer');
 
-		
+                    if ((song !== null) && (artist !== null) && (action) && (action.done)) {
+
+                        song = song.raw;
+                        artist = artist.raw;
+                        spotify.searchSongAndArtist(song, artist, function (urls) {
+                            const options = {
+                                messageText: null,
+                                title: song.raw + " -- " + artist,
+                                mainUrl: urls.song,
+                                imageUrl: urls.image,
+                                buttonType: 'web_url',
+                                buttonTitle: 'Ecouter un extrait',
+                                buttonUrl: urls.sample,
+                            };
+                            facebook.replyButton(senderID, options);
+                        });
+
+                    }
+                }
+
+            }).catch(function (err) {
+                console.error("RecastAI conversation failed!! Check the logs for more details");
+                console.error(err);
+            });
+        });
 	}
 
 	else if (messageAttachments) {
